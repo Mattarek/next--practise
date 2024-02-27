9f86eefb4fdfa2b6b8bf966f38310f7524d15022
Marcin Barc <marb637@gmail.com>
2024-02-27 18:45:17 +0100

    Analytics component added

---
+++ src/utils/analytics.ts
@@ -0,0 +1,14 @@
+interface AnalyticsArgs {
+    retention?: number;
+}
+
+export class Analytics {
+    private retention: number = 60 * 60 * 24 * 7;
+
+    constructor(opts?: AnalyticsArgs) {
+        if (opts?.retention) this.retention = opts.retention;
+    }
+}
+
+const analytics = new Analytics({ retention: 3600 });
+analytics;
